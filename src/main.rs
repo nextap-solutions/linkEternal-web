ed9d4ccc2df7565975647e29b9c2464a27dce58b
Vojtěch Šišma <v.sisma@nextap.eu>
2023-06-20 15:50:44 +0200

    init dioxus web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![allow(non_snake_case)]
+use dioxus::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    dioxus_web::launch(App);
+}
+
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello, world!"
+        }
+    })
 }
